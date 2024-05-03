d6399678d5f96540581084c4b2d082638bf3352c
Frantisek Troster <frantisek@ttzz.eu>
2024-05-03 16:23:36 +0200

    Update skriptů

--- src/MyWidgets/d.ts
+++ src/MyWidgets/d.ts
@@ -1,3 +1,4 @@
-export interface IConnWidgets {
+interface IConnWidgets {
     loadLeadCommHistory: (leadId: number, element: HTMLElement) => boolean;
+    loadClientCommHistory: (clientId: number, element: HTMLElement) => boolean;
  }
--- src/MyWidgets/script.js
+++ src/MyWidgets/script.js
@@ -71,7 +71,73 @@ const loadLeadCommHistory = async (leadId, element) => {
     return true;
 }
 
+const loadClientCommHistory = async (clientId, element) => {    
+    const commHistory = await doo.table.getData('clientcontact', {
+        filter: 'client.id(eq)' + clientId,
+        limit: 20,
+        sort: 'createdAt(desc)'
+    });
+    
+    
+    element.innerHTML = `
+    <div class="col-sm-6 col-xs-12">                
+        <ul class="communication-history">
+            
+        </ul>
+    </div>
+    <div class="col-sm-6 col-xs-12">
+        <ul class="contact-details">
+            
+        </ul>
+    </div>
+    `;
+
+    if (commHistory.data.length == 0) { 
+        element.firstElementChild.firstElementChild.innerHTML = '<li>Žádná komunikace doposud neproběhla</li>';        
+    }
+
+    const parser = new DOMParser();
+    commHistory.data.forEach(communication => {
+        const createdAtDate = new Date(communication.created).toLocaleDateString('cs-CZ', { timeZone: 'Europe/Prague' }) + ' ' + new Date(communication.created).toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' });
+        const modifiedAtDate = 'Upraveno: ' + new Date(communication.modified).toLocaleDateString('cs-CZ', { timeZone: 'Europe/Prague' }) + ' ' + new Date(communication.modified).toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' });
+        const deadlineDate = communication.fields.deadline == null ? '' : ', Deadline: ' + new Date(communication.fields.deadline).toLocaleDateString('cs-CZ', { timeZone: 'Europe/Prague' }) + ' ' + new Date(communication.fields.deadline).toLocaleTimeString('cs-CZ', { timeZone: 'Europe/Prague' });
+        let communicationTypes = "";
+
+        (Array.isArray(communication.fields.communicationType) ? communication.fields.communicationType : [communication.fields.communicationType]).forEach(type => {
+            type = type.toLowerCase();
+            if (type == "email") communicationTypes += '<i title="' + type + '" class="far fa-envelope"></i>';
+            if (type == "sms") communicationTypes += '<i title="' + type + '" class="far fa-message"></i>';
+            if (type == "telefon") communicationTypes += '<i title="' + type + '" class="far fa-phone"></i>';
+            if (type == "osobně") communicationTypes += '<i title="' + type + '" class="far fa-user-clock"></i>';
+        });
+
+        const communicationID = communication.id;
+        const communicationOutcome = communication.fields.communicationOutcome;
+        const communicationFolowUp = (communication.fields.hasOwnProperty('deadlineFollowUp') ? ' - Připomínka na: ' + new Date(communication.fields.deadlineFollowUp).toLocaleDateString('cs-CZ', { timeZone: 'Europe/Prague' }) : '');
+        const communicationNote = (communication.fields.hasOwnProperty('note') ? communication.fields.note : '');
+            
+        // Parse the element string
+        const doc = parser.parseFromString(
+            `<li>
+                <abbr title="${modifiedAtDate}  ${deadlineDate}" class="modified"><i class="far fa-calendar"></i> ${createdAtDate}</abbr>                
+                <span class="outcome">
+                    <span class="types">${communicationTypes}</span> ${communicationOutcome ?? "N/A"} ${communicationFolowUp}
+                </span>
+                ${communicationNote}
+            </li>`
+        , 'text/html');
+        doc.body.firstChild.addEventListener('click', function() {
+            doo.form.openForm('clientcontact', { 'model': { 'id': communicationID } });
+        });
+
+        element.firstElementChild.firstElementChild.appendChild(doc.body.firstChild);
+    });
+    
+    return true;
+}
+
 return {
-    loadLeadCommHistory: loadLeadCommHistory
+    loadLeadCommHistory: loadLeadCommHistory,
+    loadClientCommHistory: loadClientCommHistory
 };
 
